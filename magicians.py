716f85dc25386202ca0810027f2f568cb742f750
Bal3x <bchavez_@outlook.com>
2021-09-09 13:31:08 -0400

    Add Try if Yourself excercises

---
+++ magicians.py
@@ -0,0 +1,23 @@
+#chapter 4: working with list (looping through an entire list)
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show!")
+
+#chapter 4: Try it Yourself
+
+pizzas = ['cheese', 'peperoni', 'pineaple', 'buffalo chicken']
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza")
+
+print("Basically I love pizza.\n")
+
+animals = ['dog', 'cat', 'bird']
+for animal in animals:
+    print(f"A {animal.title()} would make a great pet.")
+
+print("Any of these animals would be a great pet!\n")
