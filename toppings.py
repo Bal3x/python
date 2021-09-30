c999928b9f74c21e2d9af9775b631a13779fefd2
Bal3x <bchavez_@outlook.com>
2021-09-29 21:54:51 -0400

    Modify toppings.py to include examples

--- toppings.py
+++ toppings.py
@@ -13,3 +13,42 @@ if requested_toppings != 'anchovies':
     print("Hold the anchovies!")
     
 print("\nFinished making your pizza!")
+
+#checking for special items
+
+requested_toppings = ['mushrooms', 'pepperoni', 'green peppers', 'pineapple', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
+
+#checking that list is not empty
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
+
+#using multiple lists
+
+available_toppings = ['mushrooms', 'olives', 'extra cheese', 'green pepeers', 'pepperoni', 'pineapple']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry we don't have {requested_topping}.")
+
+print("\nFinished making your pizza!")
+
