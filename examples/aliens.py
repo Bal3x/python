a597612bb410840dd8982ab6b1149baeebe61c3a
Bal3x <bchavez_@outlook.com>
2021-10-11 22:20:28 -0400

    Modify aliens.py to include additional examples

--- examples/aliens.py
+++ examples/aliens.py
@@ -57,4 +57,53 @@ new_points = alien_0['points']
 print(f"You just earned {new_points} points!")
 
 
+#adding new key-value pairs
 
+alien_0 = {'color': 'red', 'points': 10}
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+
+#starting with an empty dictionary
+
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+# modifying values in a dictionary
+
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}")
+
+# alien moving at different speeds
+
+alien_0 = {'x_position':0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original x position: {alien_0['x_position']}")
+
+    # move alien to the right 
+    # determine how far to move the alien based on it current speed
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+
+else:
+    x_increment = 3
+
+    # the new position is the old position plus the new position
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New x position: {alien_0['x_position']}")
+
+# removing key value pairs
