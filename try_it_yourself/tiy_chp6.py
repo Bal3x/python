b05da4234c1adc9a85e98433f49504c309199a2b
Bal3x <bchavez_@outlook.com>
2021-10-14 10:52:59 -0400

    Modify tiy.ch6.py to include exercises

---
+++ try_it_yourself/tiy_chp6.py
@@ -0,0 +1,49 @@
+# 6-1 person
+
+person = {'first_name': 'carla', 'last_name': 'dones', 'age': 28, 'city': 'new york',}
+
+print(person['first_name'].title())
+print(person['last_name'].title())
+print(person['age'])
+print(person['city'].title())
+
+# people's favorite numbers
+
+favorite_numbers = {
+    'jazmine': 8,
+    'john': 1,
+    'isabel': 5,
+    'ramon': 7,
+    'leticia': 9,
+    }
+
+number1 = favorite_numbers['jazmine']
+number2 = favorite_numbers['john']
+number3 = favorite_numbers['isabel']
+number4 = favorite_numbers['ramon']
+number5 = favorite_numbers['leticia']
+
+print(f"Jazmine's favorite number is {number1}")
+print(f"John's favorite number is {number2}")
+print(f"Isabel's favorite number is {number3}")
+print(f"Ramon's favorite number is {number4}")
+print(f"Leticia's favorite number is {number5}")
+
+# programming words learned
+
+glosary = {
+    'dictionaries': 'Storing key pairs that allow you to connect pieces.',
+    'if_statements': 'Allows you to examine the current state of a program and respond appropriately.',
+    'lists': 'Allow you to store sets of information in one place.',
+    }
+
+word1  = glosary['dictionaries']
+word2 = glosary['if_statements']
+word3 = glosary['lists']
+
+
+print(f"Dictionaries: \n{word1}")
+print(f"If Statements: \n{word2}")
+print(f"List: \n{word3}")
+
+    
