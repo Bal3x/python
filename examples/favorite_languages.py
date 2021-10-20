df27e8e2ad586787bcacc9902b20b7a1c7752500
Bal3x <bchavez_@outlook.com>
2021-10-19 22:22:19 -0400

    Modify favorite_languages.py to include additional examples

--- examples/favorite_languages.py
+++ examples/favorite_languages.py
@@ -9,3 +9,55 @@ favorite_languages = {
 
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}.")
+
+# looping thru all key-value pairs in a dictionary
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c', 
+    'edward': 'ruby', 
+    'phil': 'python',
+    }
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# looping thru all the keys in a dictionary
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c', 
+    'edward': 'ruby', 
+    'phil': 'python',
+    }
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+# Favorite languages message to friends
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c#', 
+    'edward': 'ruby', 
+    'phil': 'python',
+    }
+
+friends =  ['phil', 'sarah']
+
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language} too!")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take the poll!")
+
+
+
+
+
+
+
---
+++ examples/user.py
@@ -0,0 +1,11 @@
+# looping thru all key-value pairs
+
+user_0 = {
+    'username': 'unclerico',
+    'first': 'uncle',
+    'last': 'rico',
+    }
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
