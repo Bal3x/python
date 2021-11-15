1fd85ff85545b430ebe4e153b3fc5e302c0768d7
Bal3x <bchavez_@outlook.com>
2021-11-15 15:23:12 -0500

    add parrot.py

---
+++ examples/parrot.py
@@ -0,0 +1,9 @@
+# how to use the input() function
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+# writing clear prompts
+
+name = input("Please enter your name: ")
+print()
