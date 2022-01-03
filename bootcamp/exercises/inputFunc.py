8bead3a6e05436b5ca9b55f57fef6a99645f05a3
bal3x <bchavez_@outlook.com>
2022-01-02 21:33:41 -0500

    Add files to bootcamp

---
+++ bootcamp/exercises/inputFunc.py
@@ -0,0 +1,12 @@
+#using input 
+#examples
+# input("What is you name? ")
+
+#nesting input
+# print("Hello " + input("What is your name?") + "!")
+
+
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+print(len(input("What is your name? ")))
+
+
---
+++ bootcamp/exercises/variables.py
@@ -0,0 +1,38 @@
+# working with variables
+# name = "Jack"
+# print(name)
+
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+# c = b
+# b = a 
+# a = c
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+# 1. Create a greeding for your program
+print("Welcome to Band Name Generator program.")
+
+# 2. Ask user the name of the city they grew up in.
+city = input("What city you grew up in?\n")
+
+# 3. Ask user the name of their pet.
+pet = input("What is the name of your pet?\n")
+
+# 4. Combine the name of their city and pet to generate their band name.
+print("Your band name is " + city + " " + pet)
